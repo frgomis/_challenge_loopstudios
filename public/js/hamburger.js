24d53ba40381166240f7472d16e97c3bb345853d
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-06-09 11:41:03 +0200

    menu hamburger en cours

---
+++ public/js/hamburger.js
@@ -0,0 +1,11 @@
+// alert("coucou")
+let btnHamburger = document.querySelector('.menuBurger');
+let btnClosed = document.querySelector('.icon');
+
+console.log(btnHamburger, btnClosed);
+btnHamburger.addEventListener('click', ()=>{
+    document.getElementById("sidebar").classList.toggle("active");
+})
+btnClosed.addEventListener('click', ()=>{
+    document.getElementById('sidebar').classList.toggle("closed");
+})
