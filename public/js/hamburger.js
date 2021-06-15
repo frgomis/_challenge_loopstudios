369a8da4c64ffa40ede4fe47cd03117b38d7b650
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-06-15 13:41:52 +0200

    WIP responsivee

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -2,7 +2,8 @@
 let burger = document.querySelector('.burger');
 let menuList = document.querySelector('.menu_list');
 let menu1 = document.querySelector('.menu_list');
-let nav = document.querySelector('.navbar')
+let nav = document.querySelector('.navbar');
+
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
@@ -11,10 +12,12 @@ burger.addEventListener('click', () => {
 
 // au scroll, la div.entete devient transparente
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 150 && window.innerWidth<769) {
         nav.classList.add('scroll');
     } else {
         nav.classList.remove('scroll');
     }
 
-})
+});
+// au delà de 769px, la ul.menu_list se met en display: flex 
+
