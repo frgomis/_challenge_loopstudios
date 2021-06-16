05952e3c95db8b999a8bacd539eb41e9c485837a
Francoise Gomis <gomisfrancoise@yahoo.com>
2021-06-17 00:12:15 +0200

    menu_list en mode desktop

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -2,6 +2,8 @@
 let burger = document.querySelector('.burger');
 let menuList = document.querySelector('.menu_list');
 let nav = document.querySelector('.navbar');
+let windowX = window.innerWidth;
+console.log(windowX);
 
 
 burger.addEventListener('click', () => {
@@ -12,11 +14,17 @@ burger.addEventListener('click', () => {
 // au scroll, la div.entete devient transparente
 window.addEventListener('scroll', () => {
     if (window.scrollY > 150){
-        nav.classList.add('scroll');
+        nav.classList.toggle('disparait');
     } else {
-        nav.classList.remove('scroll');
+        nav.classList.remove('disparait');
     }
 
 });
 // au delà de 769px, la ul.menu_list se met en display: flex 
 
+window.addEventListener('onresize', () => {
+    if (window.onresize > 769) {
+        burger.classList.toggle('hidden');
+        menuList.classList.toggle('hidden');
+    }
+});
